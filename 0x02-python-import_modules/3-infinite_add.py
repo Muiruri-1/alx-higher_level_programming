8156dc014a2391fb562e2b23dab19b1b8a28ad38
Muiruri-1 <muirurijosphat34@gmail.com>
2023-11-02 14:59:08 -0700

    import and modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,6 +5,6 @@ def add_arguments(*args):
     return sum(map(int, args))
 
 if __name__ == "__main__":
-    arguments = sys.argv[1:]  # Get all command-line arguments except the script name
+    arguments = sys.argv[1:]
     result = add_arguments(*arguments)
     print(result)
