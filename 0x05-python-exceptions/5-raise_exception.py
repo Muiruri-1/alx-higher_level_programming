fc0bd4d2a2d4a3cc7f04a6d47bc104c8287ef002
Muiruri-1 <muirurijosphat34@gmail.com>
2023-11-20 15:39:01 -0800

    exceptions

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def raise_exception():
-    try:
-        # Raise a custom type exception
-        raise TypeError("This is a custom type exception")
-    except TypeError as e:
-        # Handle the exception and print a custom message
-        print(e)
+    raise TypeError
