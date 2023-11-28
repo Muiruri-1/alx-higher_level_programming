8fa8af459c434c1a85abdc6bc85f91067be78209
Muiruri-1 <muirurijosphat34@gmail.com>
2023-11-27 16:37:03 -0800

    python - more classes

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+class Rectangle:
+    """
+    Empty class that defines a rectangle.
+    """
+    pass
+
+
+# Example usage:
+# Instantiate an object of the Rectangle class
+my_rectangle = Rectangle()
+
+# You can add attributes or methods to the class as needed in the future.
