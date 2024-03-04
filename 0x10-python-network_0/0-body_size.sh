b1dbf79d253730de521309eae1e4bb9c3bc156fc
Muiruri-1 <muirurijosphat34@gmail.com>
2024-03-04 09:27:42 -0800

    python network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
+# This script takes a URL as input, sends a request using curl and displays the size in bytes
+curl -sI "$1" | grep -i content-length | awk '{print $2}'
 
-# Check if a URL argument is provided
-if [-z "$1"]; then
-	echo "Error: Please provide a URL as an argument."
-	exit 1
-fi
-
-# URL from the first argument
-url="$1"
-
-# Get the response header only with the -sI option and pipe it to grep for Content-Length
-size=$(curl -sI "$url" | grep -i Content-Length | awk '{print $2}')
-
-# Check if size is empty (not found in header)
-if [ -z "$size" ]; then
-	echo "Error: Content-Length not found in response header."
-else
-	echo "Response size for $url: $size bytes"
-fi
