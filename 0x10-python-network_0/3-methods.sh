f5e244cae99f51e22502e0f821985d9200c49166
Muiruri-1 <muirurijosphat34@gmail.com>
2024-03-04 10:07:27 -0800

    python network

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes a URL as input and displays all HTTP methods the server will accept
-curl -s -X OPTIONS "$1" | grep -i Allow | awk '{print $2}'
+curl -sI -X OPTIONS "$1" | grep -i allow | cut -d ":" -f 2-
